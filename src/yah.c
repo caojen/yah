008685ce1c6236f835139c581e6cc9c6fcedd4ec
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-05-30 19:53:52 +0800

    feat: add usage

--- src/yah.c
+++ src/yah.c
@@ -5,8 +5,10 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "yah.h"
+#include "yah_const.h"
 
 int
 main(int argc, char* argv) {
@@ -23,8 +25,8 @@ main(int argc, char* argv) {
         } else if(strcmp(argv[i], "reload") == 0) {
             /* reload config */
             /* send SIGHUP to daemon */
-        } else if(strcmp(argv[i], "reset") == 0) {
-            /* reset a key */
+        } else if(strcmp(argv[i], "set") == 0) {
+            /* set a key */
             /* set the key to config */
             /* then send SIGHUP to daemon */
         } else {
@@ -36,5 +38,10 @@ main(int argc, char* argv) {
 
 void
 yah_usage(void) {
-
+    printf("Yah -- v1.0\n");
+    printf(" Usage: Yah [start | reload | set] <...>\n\n");
+    printf(" start: start daemon. If the daemon is started, error.\n");
+    printf(" reload: reload settings from config file: %s\n", YAH_CONFFILE);
+    printf(" set <key> <value>: set setting. It will update config file.\n");
+    printf("   'set' and 'reload' try to change the daemon. It the daemon is not started, error.\n");
 }
