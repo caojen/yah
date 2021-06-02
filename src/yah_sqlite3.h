00b20e6d1a75d01e7aa1de6fd127dd45afb6c083
caojen <caojen@mail2.sysu.edu.cn>
2021-06-02 11:36:34 +0800

    init: function clarify

--- src/yah_sqlite3.h
+++ src/yah_sqlite3.h
@@ -43,4 +43,44 @@ typedef int (*sqlite3_callback_f)(void* callback, int argc, char** argv, char** 
 // using callback to catch all response
 #define yah_sqlite_select(db, sql, callback, arg) yah_sqlite_exec(db, sql, callback, arg)
 
+#include "yah_airodump.h"
+/**
+ * all these function will use the global variable sqlite* db
+ * extern the variable to all other components
+ */
+
+extern sqlite3* db;
+
+// open the database
+// store db into the global pointer 'db'
+// return 0(SQLITE_OK) if success, ignore all callbacks
+int yah_open_database();
+
+// close the database
+// after closing, the pointer db will be set to NULL
+// return 0(SQLITE_OK) if success, ignore all callbacks
+int yah_close_database();
+
+// insert data into db
+// return 0(SQLITE_OK) if success, ignore the callback
+// auto generate data->data.id if success
+int yah_airodump_data_insert(struct yah_airodump_data* data);
+
+// set data in data to 'updated'
+// return 0(SQLITE_OK) if success, ignore the callback
+int yah_airodump_data_updated(struct yah_airodump_data* data);
+
+// remove all data that will not be used
+// using the const YAH_AP_TIME and YAH_APSTATION_TIME
+// to determine which rows will be deleted
+// return 0(SQLITE_OK) if success, ignore the callback
+int yah_airodump_data_de_redundancy();
+
+// fetch all un-updated data from database
+// store all rows into param struct yah_airodump_data** data
+// the size of data is stored into unsigned* size
+// if the data is empty, size will be set to 0, data will be null
+// return 0(SQLITE_OK) if success, ignore other callbacks
+int yah_airodump_data_fetch_unupdated(struct yah_airodump_data** data, unsigned* size);
+
 #endif
