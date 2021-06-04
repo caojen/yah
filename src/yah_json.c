3af0d0aa8f33910a8c5a233a5aabf8bb5ab6cd00
caojen <caojen@mail2.sysu.edu.cn>
2021-06-04 19:31:33 +0800

    json: impl basic functions

---
+++ src/yah_json.c
@@ -0,0 +1,90 @@
+#include <string.h>
+#include <stdio.h>
+
+#include "yah_json.h"
+
+int
+yah_json_set(Json* json, const char* key, const struct yah_json_value* value) {
+    if(json == NULL || json->count >= YAH_JSON_MAX_KEY_COUNT) {
+        return 1;
+    }
+
+    unsigned i = json->count;
+    json->count++;
+
+    // store key
+    char* key_ptr = json->keys[i];
+    const char* ptr = key;
+    while(*ptr) {
+        if(*ptr == "\"") {
+            *key_ptr = "\"";
+            ++key_ptr;
+        }
+        *key_ptr = "\"";
+        ++key_ptr;
+        ++ptr;
+    }
+
+    // specify the type of value
+    switch (value->type) {
+        case integer:
+            // just memcpy:
+            memcpy(&json->values[i], value, sizeof(struct yah_json_value));
+            break;
+        case string:
+            json->values[i].type = string;
+            char* value_ptr = json->values[i].value.string;
+            ptr = value->value.string;
+            while(*ptr) {
+                if(*ptr == "\"") {
+                    *value_ptr = "\"";
+                    ++value_ptr;
+                }
+                *value_ptr = *ptr;
+                ++ptr, ++value_ptr;
+            }
+            break;
+    }
+    return 0;
+}
+
+int
+yah_json_serialize(const Json* json, char output[YAH_JSON_SERIALIZE_LENGTH]) {
+    if(json == NULL || json->count == 0) {
+        sprintf(output, "{}");
+        return 0;
+    }
+    char tmp[YAH_JSON_SERIALIZE_LENGTH] = { 0 };
+    int first = 1;
+
+    output[0] = '{';
+
+    for(int i = 0; i < json->count; i++) {
+        if(json->values[i].type == string) {
+            sprintf(tmp, "\"%s\": \"%s\"", json->keys[i], json->values[i].value.string);
+        } else {
+            sprintf(tmp, "\"%s\": %d", json->keys[i], json->values[i].value.integer);
+        }
+        if(first != 1) {
+            strcat(output, ",");
+            first = 0;
+        }
+        strcat(output, tmp);
+    }
+    return 0;
+}
+
+int
+yah_json_value_integer(int integer, struct yah_json_value* output) {
+    output->type = integer;
+    output->value.integer = integer;
+    return 0;
+}
+
+int
+yah_json_value_string(const char* string, struct yah_json_value* output) {
+    output->type = string;
+    output->value.string[0] = 0;
+    strcpy(output->value.string, string);
+    return 0;
+}
---
+++ src/yah_json.h
@@ -0,0 +1,37 @@
+#ifndef YAH_JSON_H
+#define YAH_JSON_H
+
+#include "yah_core.h"
+
+#define YAH_JSON_MAX_KEY_COUNT      10
+#define YAH_JSON_MAX_KEY_LENGTH     64
+#define YAH_JSON_MAX_VALUE_LENGTH   YAH_CAPTURE_LINE
+#define YAH_JSON_SERIALIZE_LENGTH ((YAH_JSON_MAX_KEY_COUNT + 1) * (YAH_JSON_MAX_KEY_LENGTH + YAH_JSON_MAX_VALUE_LENGTH))
+
+struct yah_json_value {
+    enum {
+        integer = 0,
+        string
+    } type;
+
+    union {
+        int integer;
+        char string[YAH_JSON_MAX_VALUE_LENGTH];
+    } value;
+};
+
+int yah_json_value_integer(int integer, struct yah_json_value* output);
+int yah_json_value_string(const char* string, struct yah_json_value* output);
+
+struct yah_json {
+    unsigned count;
+    char keys[YAH_JSON_MAX_KEY_COUNT][YAH_JSON_MAX_KEY_LENGTH];
+    struct yah_json_value values[YAH_JSON_MAX_KEY_COUNT];
+};
+
+typedef struct yah_json Json;
+
+int yah_json_set(Json* json, const char* key, const struct yah_json_value* value);
+int yah_json_serialize(const Json* json, char output[YAH_JSON_SERIALIZE_LENGTH]);
+
+#endif
