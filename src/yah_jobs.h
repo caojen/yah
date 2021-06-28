82bbf34b47af407b51d3af39a022f0b6c8d12d0f
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-06-28 17:52:00 +0800

    job: init

--- src/yah_jobs.h
+++ src/yah_jobs.h
@@ -5,5 +5,9 @@
 struct yah_airodump_data;
 void yah_rp_pool_job_func_ap(struct yah_airodump_data* data);
 void yah_rp_pool_job_func_apstation(struct yah_airodump_data* data);
+int yah_rp_pool_job_send_data(char* final, char* url);
+// data is a array of pointer, ended with a nullptr
+void yah_rp_pool_job_func_aps(struct yah_airodump_data** data);
+void yah_rp_pool_job_func_apstations(struct yah_airodump_data** data);
 
 #endif
