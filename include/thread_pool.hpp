5275190cedf3cc42af92edb4ee3e6716d9a5c651
caojen <caojen@mail2.sysu.edu.cn>
2021-07-26 22:40:03 +0800

    init threadpool

---
+++ include/thread_pool.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+// 一个典型的线程池
+// 线程池包含两个部分，一个是 num_workers 个工作线程
+// 另外一个是一个队列，队列中包含的元素是 T 类型的，并且，类型T需要实现 run 方法，在这里我们用基类Job代替T
+// 线程池将获得Job的所有权，因此，使用unique_ptr来保存数据
+
+#include <thread>
+#include <memory>
+#include <mutex>
+
+namespace yah {
+  class Job;
+
+  class ThreadPool {
+    public:
+      // 什么都不做的构造函数
+      ThreadPool();
+
+      // 指明工作线程数量
+      // 当线程池构造完成后，所有线程自动被构建，并且进行工作
+      ThreadPool(size_t num_workers);
+
+      // 将一个工作推入到线程中
+      void push();
+  };
+}
