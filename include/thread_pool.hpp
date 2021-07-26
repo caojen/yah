131d21e9d900c62121dd7b81c528fe21fcf0573a
caojen <caojen@mail2.sysu.edu.cn>
2021-07-27 00:36:59 +0800

    temp save before auto pool

--- include/thread_pool.hpp
+++ include/thread_pool.hpp
@@ -5,11 +5,15 @@
 // 另外一个是一个队列，队列中包含的元素是 T 类型的，并且，类型T需要实现 run 方法，在这里我们用基类Job代替T
 // 线程池将获得Job的所有权，因此，使用unique_ptr来保存数据
 
+// AutoPool
+// 与线程池相反，自动池只接受数据，然后池中的线程就会对这些数据进行批量处理
+
 #include <thread>
 #include <memory>
 #include <mutex>
 #include <queue>
 #include <condition_variable>
+#include <functional>
 
 namespace yah {
   class Job;
@@ -33,6 +37,26 @@ namespace yah {
       std::mutex                              mutex;
       // 条件变量
       std::condition_variable                 cv;
+  };
+
+  template<class T>
+  class AutoPool {
+    public:
+      // 什么都不做的构造函数
+      AutoPool();
+
+      // 指明工作线程数量
+      // 指明每次工作最大处理数量
+      // 指明工作内容
+      AutoPool(size_t num_workers, size_t num_per_time, std::function<void(void)> func);
+
+      void push(std::unique_ptr<T>& one_data);
+    private:
+      std::queue<std::unique_ptr<T>>        data;
+      // 多线程控制互斥锁
+      std::mutex                            mutex;
+      // 条件变量
+      std::condition_variable               cv;
   };
 
   class Job {
