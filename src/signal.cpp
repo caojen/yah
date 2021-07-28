d12b2f5d74f8e4f9f5547271cd741f779e80f890
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-07-28 12:54:39 +0800

    registe sigchld and sigterm

--- src/signal.cpp
+++ src/signal.cpp
@@ -9,10 +9,28 @@ namespace yah {
     }
   }
 
+  static void sigchld_handler(int signo, siginfo_t* info, void* context) {
+    pid_t child = info->si_pid;
+    log << ctime << "[SIGCHLD] Receive From Child Pid = " << child << endl;
+    log << ctime << "[SIGCHLD] Current Airodump Child Pid is " << airodump_pid << endl;
+    if(airodump_pid != 0 && airodump_pid == child) {
+      log << ctime << "[SIGCHLD] Matched. It seems that Airodump-ng has exited. Abort.";
+      abort();
+    }
+  }
+
+  static void sigterm_handler(int signo, siginfo_t* info, void* context) {
+    log << ctime << "[SIGTERM] Received. Tring to abort." << endl;
+    log << ctime << "[SIGTERM] Current Airodump Child Pid is " << airodump_pid << endl;
+    kill(airodump_pid, SIGTERM);
+    log << ctime << "[SIGTERM] Sent SIGTERM to Child " << airodump_pid << endl;
+    log << ctime << "[SIGTERM] Aborted." << endl;
+  }
+
   void SignalHandler::handle() {
     log << ctime << "[SignalHandler]" << endl;
-    auto signals = { SIGHUP };
-    auto handlers = { sighup_handler };
+    auto signals = { SIGHUP, SIGCHLD, SIGTERM };
+    auto handlers = { sighup_handler, sigchld_handler, sigterm_handler };
     struct sigaction sa;
 
     for(auto i = 0; i < handlers.size(); i++) {
