ab701c89880a23eb1aae57d8ff8235195f3376eb
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2022-08-01 22:38:03 +0800

    debug: limit

--- src/checker.cpp
+++ src/checker.cpp
@@ -33,12 +33,16 @@ namespace yah {
       return;
     }
 
+    printf("this->ptr->get_pwr() is %d, where limit is %d\n", this->ptr->get_pwr(), limit);
+
     if (this->ptr->get_pwr() >= limit) {
       auto time = this->ptr->get_create_time();
       this->ptr->set_create_time(to_next_time(time));
+      printf("ok, pass to cache and db\n");
 
       if(!this->ptr->in_cache() && !this->ptr->in_db()) {
         this->ptr->sync_db();
+        printf("ok, pass to sender\n");
         // 持久化完成，交给sender
         sender->push(std::move(this->ptr));
       }
